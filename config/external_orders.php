88dadf1be81ad286af49e1867b038f74fa71a8d2
Leo Oliveira <leo@13dev.pt>
2025-08-05 17:51:49 +0100

    feat: create config for endpoints

---
+++ config/external_orders.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return [
+    'v1' => [
+        'uri' => env('EXTERNAL_ORDERS_API_V1_URI', null)
+    ],
+    'v2' => [
+        'uri' => env('EXTERNAL_ORDERS_API_V2_URI', null)
+    ]
+];
